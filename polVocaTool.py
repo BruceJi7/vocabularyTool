fa07d2fd6f9f41f0e15bfc3495870be9e7a01c77
Tobias S <44494591+BruceJi7@users.noreply.github.com>
2019-10-01 12:17:18 +0900

    added search skip for phrases to Polish tool.
    Type in an anki card in full and use * to skip search.
    Writes the skipped card to a separate phrase card file.

--- polVocaTool.py
+++ polVocaTool.py
@@ -13,73 +13,82 @@ polDictURL = r'https://www.diki.pl/slownik-angielskiego?q='
 def getDefinitionsForPolishWords(listOfPolishWords):
 
     newAnkiCards = []
+    newPhraseCards = []
 
     for word in listOfPolishWords:
-        try:
-            print(f'Searching for word {word}...')
-            searchWord = word #Placeholder for testing, replace with function variable
-            searchURL = polDictURL + searchWord
-
-            # webbrowser.open_new_tab(searchURL)
-
-            res = req.get(searchURL, headers=headers)
-            res.raise_for_status()
-
-            soup = bs4.BeautifulSoup(res.text, features='lxml')
+        if word[0] == '*':
+            newPhraseCards.append(word[1:])
+        else:
             try:
-                allDefs = soup.findAll('span', class_="hw")
-            except:
-                print('Error 1: BS4 Parsing failed at first search')
+                print(f'Searching for word {word}...')
+                searchWord = word #Placeholder for testing, replace with function variable
+                searchURL = polDictURL + searchWord
 
-            try:
-                polishSourceWord = allDefs[0].get_text().capitalize()
-                print(f'Polish root word is: {polishSourceWord}')
-                firstEngDef = allDefs[1].find('a', class_="plainLink")
-                
+                # webbrowser.open_new_tab(searchURL)
+
+                res = req.get(searchURL, headers=headers)
+                res.raise_for_status()
+
+                soup = bs4.BeautifulSoup(res.text, features='lxml')
                 try:
-                    engDef = firstEngDef.get_text()
-                    if 'ć' in engDef.lower():
-                        print(f'Found Polish verb: {engDef}. Verb added to source word list')
-                        listOfPolishWords.append(engDef)
-                        continue
-                    else:
-                        print(f'Found definition: {engDef}')
+                    allDefs = soup.findAll('span', class_="hw")
+                except:
+                    print('Error 1: BS4 Parsing failed at first search')
+
+                try:
+                    polishSourceWord = allDefs[0].get_text().capitalize()
+                    print(f'Polish root word is: {polishSourceWord}')
+                    firstEngDef = allDefs[1].find('a', class_="plainLink")
+                    
                     try:
-                        newAnkiCards.append(f'{polishSourceWord}:{engDef.capitalize()}')
+                        engDef = firstEngDef.get_text()
+                        if 'ć' in engDef.lower():
+                            print(f'Found Polish verb: {engDef}. Verb added to source word list')
+                            listOfPolishWords.append(engDef)
+                            continue
+                        else:
+                            print(f'Found definition: {engDef}')
+                        try:
+                            newAnkiCards.append(f'{polishSourceWord}:{engDef.capitalize()}')
+                        except:
+                            print('Failed to write to file.')
+
                     except:
-                        print('Failed to write to file.')
+                        print('Failed to extract text')
+
 
                 except:
-                    print('Failed to extract text')
-
-
+                    print('Definition not found. BS4 Parsing error?')
+            
+                    
             except:
-                print('Definition not found. BS4 Parsing error?')
-        
-                
-        except:
-            print(f'Error finding {word}: not in dictionary?')
+                print(f'Error finding {word}: not in dictionary?')
 
 
 
-    return newAnkiCards
+    return newAnkiCards, newPhraseCards
+
 
 ### META FUNCTIONS
 
 # makeNewPolAnkiCards - take words from .txt file, search for definitions, format results for anki and write.
 def makeNewPolAnkiCards():
     newPolishWords = getPolishWords()
-    cardFormattedPolWords = getDefinitionsForPolishWords(newPolishWords)
+    cardFormattedPolWords, phraseCards = getDefinitionsForPolishWords(newPolishWords)
         
     resetPolishFile()
 
     vocaTools.writeUTFCardDump(cardFormattedPolWords, 'newPolishCards')
+    if phraseCards:
+        resetPolPhraseFile()
+        vocaTools.writeUTFCardDump(phraseCards, 'newPolishPhrases')
 
 #Retrieve words from dropbox file
 getPolishWords =vocaTools.getWords(dropBoxPolFile)
 
 #Reset anki file to prevent repeat cards
 resetPolishFile = vocaTools.resetTextFile('newPolishCards')
+resetPolPhraseFile = vocaTools.resetTextFile('newPolishPhrases')
 
 #Perform main task - produce cards
 makeNewPolAnkiCards()
